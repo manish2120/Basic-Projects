2ba5b3bb8ebae84d183564e253e77c4499f36f8d
Manish Chavan <145777097+manish2120@users.noreply.github.com>
2024-01-28 16:52:38 +0530

    Update script.js

--- To-do-list/script.js
+++ To-do-list/script.js
@@ -2,16 +2,35 @@ const taskInput = document.getElementById("taskInput");
 const addBtn = document.getElementById("addBtn");
 const tasks = document.querySelector(".list-container");
 
+//input event listeners to check the input value
+taskInput.addEventListener("input", () => {
+  const inputVal = taskInput.value.trim();
+
+  if (inputVal === "") {
+    addBtn.setAttribute("disabled", true);
+  } else {
+    addBtn.removeAttribute("disabled");
+  }
+});
+
 addBtn.addEventListener("click", () => {
+  const inputVal = taskInput.value.trim();
+
+  if (inputVal === "") {
+    addBtn.setAttribute("disabled", true);
+    return;
+  }
+
+  addBtn.removeAttribute("disabled");
+
   let taskList = document.createElement("li");
-  const inputVal = taskInput.value;
-  taskList.innerHTML = `<span>${inputVal}</span>
-  <button class="btns border ms-2 completeBtn">Complete</button>
-  <button class="btns border ms-2 delBtn">Delete</button>`;
+  taskList.innerHTML = `<div>${inputVal}</div>
+    <div class="taskBtns">
+      <button class="btns border-0 ms-2 completeBtn">Complete</button>
+      <button class="btns border-0 ms-2 delBtn">Delete</button>
+    </div>`;
 
-  let lists = tasks.appendChild(taskList);
-
-  lists.style.backgroundColor = "#424769";
+  tasks.appendChild(taskList);
 
   const completeBtn = taskList.querySelector(".completeBtn");
   const delBtn = taskList.querySelector(".delBtn");
@@ -19,12 +38,11 @@ addBtn.addEventListener("click", () => {
   completeBtn.addEventListener("click", (element) => {
     const btn = element.target;
     btn.innerText = btn.innerText === "Complete" ? "Completed" : "Complete";
-    btn.classList.toggle("changeColor");
+    btn.parentElement.parentElement.classList.toggle("changeColor");
   });
 
   delBtn.addEventListener("click", (element) => {
-    console.log(element);
     const btn = element.target;
-    btn.parentElement.style.display = "none";
+    btn.parentElement.parentElement.style.display = "none";
   });
 });
