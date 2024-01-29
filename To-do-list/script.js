a337ce5630194a214f4f28aa7232165be95a5e6c
Manish Chavan <145777097+manish2120@users.noreply.github.com>
2024-01-29 15:28:54 +0530

    Added Tasks Count

--- To-do-list/script.js
+++ To-do-list/script.js
@@ -1,8 +1,11 @@
 const taskInput = document.getElementById("taskInput");
 const addBtn = document.getElementById("addBtn");
 const tasks = document.querySelector(".list-container");
+const listCount = document.querySelector(".listCount");
 
-//input event listeners to check the input value
+let count = 0;
+
+//input event listener to check the input value
 taskInput.addEventListener("input", () => {
   let inputVal = taskInput.value.trim();
 
@@ -15,41 +18,49 @@ taskInput.addEventListener("input", () => {
 
 addBtn.addEventListener("click", () => {
   const inputVal = taskInput.value.trim();
-  
+
   if (inputVal === "") {
     addBtn.setAttribute("disabled", true);
     return;
   }
-  
+
   addBtn.removeAttribute("disabled");
-  
-  
+
   let taskList = document.createElement("li");
   taskList.innerHTML = `<div>${inputVal}</div>
   <div class="taskBtns">
   <button class="btns border-0 ms-2 completeBtn">Complete</button>
   <button class="btns border-0 ms-2 delBtn">Delete</button>
   </div>`;
-  
-  tasks.appendChild(taskList);
-  
-  taskInput.value = ""; 
 
-    if(tasks.childNodes.length > 0) {
+  tasks.appendChild(taskList);
+
+  count++;
+  updateListCount();
+
+  function updateListCount() {
+    listCount.innerText = `Total tasks : ${count}`;
+    if (count > 0) {
       tasks.style.padding = "1rem";
+    } else {
+      tasks.style.padding = "0";
     }
-    
+  }
+
+  taskInput.value = "";
+
   const completeBtn = taskList.querySelector(".completeBtn");
   const delBtn = taskList.querySelector(".delBtn");
 
   completeBtn.addEventListener("click", (element) => {
     const btn = element.target;
     btn.innerText = btn.innerText === "Complete" ? "Completed" : "Complete";
-    btn.parentElement.parentElement.classList.toggle("changeColor");
+    taskList.classList.toggle("changeColor");
   });
 
-  delBtn.addEventListener("click", (element) => {
-    const btn = element.target;
-    btn.parentElement.parentElement.style.display = "none";
+  delBtn.addEventListener("click", () => {
+    tasks.removeChild(taskList);
+    count--;
+    updateListCount();
   });
 });
